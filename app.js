a9ae4bf19550ea5e04be8766fef46772792ee5b2
SUNNY DEV <sunnydev4589@gmail.com>
2021-12-01 13:00:59 +0530

    adding files

---
+++ app.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParse = require("cookie-parser");
+const cors = require("cors");
+
+const app = express();
+
+//DB Connection
+mongoose.connect(process.env.DATABASE, { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(() => {
+    console.log("Connected to DB")
+});
+
+//Middlewares
+app.use(bodyParser.json());
+app.use(cookieParse());
+app.use(cors());
+
+//Routes
+app.use("/api", authRoutes);
+
+//Port
+const port = process.env.PORT || 8000;
+
+//Spinning a server
+app.listen(port, () => {
+    console.log(`Server up and running on port ${port}`)
+});
+
