ea775294c64f699537e3eedb09c86150fe5070b2
SUNNY DEV <sunnydev4589@gmail.com>
2021-12-28 22:42:24 +0530

    adding signup validations

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,16 @@
-const User = require("../models/user")
+const User = require("../models/user");
+const { check, validationResult } = require('express-validator');
 
 exports.signup = (req, res) => {
+
+    const errors = validationResult(req)
+
+    if(!errors.isEmpty()){
+        return res.status(422).json({
+            error: errors.array()[0].msg
+        })
+    }
+
     const user = new User(req.body)
     user.save((err, user) => {
         if(err){
@@ -15,7 +25,7 @@ exports.signup = (req, res) => {
 
 exports.signout = (req, res) => {
     res.json({
-        message: "User signout`"
+        message: "User signout"
     }) 
 };
 
